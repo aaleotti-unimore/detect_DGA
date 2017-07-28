d16d68f83db977384507c1e10399307fd9edc36f
Alessandro Aleotti <71196@studenti.unimore.it>
2017-07-28 18:14:26 +0200

    script per il dw dei datasets

---
+++ get_dga_sets.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget -c --tries=10 https://github.com/andrewaeva/DGA/archive/master.zip
+unzip master.zip 'DGA-master/all_dga.txt'
---
+++ get_top_million.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget -c --tries=10 http://downloads.majesticseo.com/majestic_million.csv -P training_set/
