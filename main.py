967b5cd33b0aeef222c4401f84283938f33c0717
Alessandro Aleotti <71196@studenti.unimore.it>
2017-08-24 16:28:35 +0200

    plot

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def normal_training():
         logger.info("\n\n%s" % classification_report(y_test, y_pred, target_names=['Benign', 'DGA'], ))
 
 
-def roc_comparison():
+def roc_comparison(graphic=True):
     tprs = []
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
@@ -134,17 +134,44 @@ def roc_comparison():
         mean_auc = auc(mean_fpr, mean_tpr)
         std_auc = np.std(aucs)
         colors = ['r', 'g', 'b']
-        plt.plot(mean_fpr, mean_tpr, color=colors[index],
-                 label=r'%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (clf_name, mean_auc, std_auc),
-                 lw=2, alpha=.8)
+
         # std dev plot
         std_tpr = np.std(tprs, axis=0)
         tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
         tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-        plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color=colors[index], alpha=.2,
-                         label=r'$\pm$ 1 std. dev.')
 
-    # plot definition
+        if graphic:
+            plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color=colors[index], alpha=.2,
+                             label=r'$\pm$ 1 std. dev.')
+            plt.plot(mean_fpr, mean_tpr, color=colors[index],
+                     label=r'%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (clf_name, mean_auc, std_auc),
+                     lw=2, alpha=.8)
+        else:
+            graphic_datas = [mean_tpr, mean_fpr, tprs_lower, tprs_upper, clf_name, mean_auc, std_auc]
+            joblib.dump(graphic_datas, "graphic_datas.pkl")
+
+    if graphic:
+        # plot definition
+        plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+                 label='Luck', alpha=.8)
+        plt.xlim([-0.05, 1.05])
+        plt.ylim([-0.05, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        # plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        plt.savefig("plot.png")
+        plt.show()
+
+
+def plot_data():
+    colors = ['r', 'g', 'b']
+    mean_tpr, mean_fpr, tprs_lower, tprs_upper, clf_name, mean_auc, std_auc = joblib.load("graphic_datas.pkl")
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color=colors[0], alpha=.2,
+                     label=r'$\pm$ 1 std. dev.')
+    plt.plot(mean_fpr, mean_tpr, color=colors[0],
+             label=r'%s Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (clf_name, mean_auc, std_auc),
+             lw=2, alpha=.8)
     plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
              label='Luck', alpha=.8)
     plt.xlim([-0.05, 1.05])
@@ -154,7 +181,7 @@ def roc_comparison():
     # plt.title('Receiver operating characteristic example')
     plt.legend(loc="lower right")
     plt.savefig("plot.png")
-    # plt.show()
+    plt.show()
 
 
 def grid_search():
